289ec844b0f17912bcafac24f2af07ac92cdcdba
ingdanielb <danielb2889@hotmail.com>
2021-08-11 08:52:43 -0500

    constructor de clase

--- POO/Persona.cs
+++ POO/Persona.cs
@@ -8,10 +8,24 @@ namespace POO
         public double Estatura { get; set; }
         public double Peso { get; set; }
         public int Salario { get; set; }
+       
+
+        //Contructor
+        public Persona(string nombre, string apellido, int edad, double estatura, double peso, int salario){
+            Nombre = nombre;
+            Apellido = apellido;   
+            Edad = edad;
+            Estatura = estatura;
+            Peso = peso;
+            Salario = salario;         
+        }
+
+        public Persona(){}        
 
 
         //Metodos
-        public void infoPersona(){
+        public void infoPersona()
+        {
             System.Console.WriteLine("Nombre: " + Nombre);
             System.Console.WriteLine("Apellido: " + Apellido);
             System.Console.WriteLine("Edad: " + Edad);
@@ -19,6 +33,48 @@ namespace POO
             System.Console.WriteLine("Peso: " + Peso);
             System.Console.WriteLine("Salario: " + Salario);
         }
-        
+
+        public double indiceMasaC()
+        {
+            double imc = Peso / (Estatura * Estatura);
+            return imc;
+        }
+
+        public void alertaIMC(double imc)
+        {
+            if (imc < 18.5)
+            {
+                System.Console.WriteLine("Bajo peso");
+            }
+            else if (imc >= 18.5 && imc <= 24.9)
+            {
+                System.Console.WriteLine("Peso normal");
+            }
+            else if (imc >= 25 && imc <= 26.9)
+            {
+                System.Console.WriteLine("Sobrepeso I");
+            }
+            else if (imc >= 27 && imc <= 29.9)
+            {
+                System.Console.WriteLine("Sobrepeso II");
+            }
+            else if (imc >= 30 && imc <= 34.9)
+            {
+                System.Console.WriteLine("Obesidad I");
+            }
+            else if (imc >= 35 && imc <= 39.9)
+            {
+                System.Console.WriteLine("Obesidad II");
+            }
+            else if (imc >= 40 && imc <= 49.9)
+            {
+                System.Console.WriteLine("Obesidad III (morbida)");
+            }
+            else if (imc >=50){
+                System.Console.WriteLine("Obesidad IV (extrema)");
+            }
+
+        }
+
     }
 }
--- POO/Program.cs
+++ POO/Program.cs
@@ -11,11 +11,32 @@ namespace POO
             persona1.Apellido = "Perez";
             persona1.Edad = 30;
             persona1.Estatura = 1.75;
-            persona1.Peso = 65.3;
-            persona1.Salario = 3000000; 
-            
+            persona1.Peso = 80.7;
+            //persona1.Salario = 3000000; 
+
             persona1.infoPersona();
 
+            Persona persona2 = new Persona();
+            persona2.Nombre = "Pepa";
+            persona2.Apellido = "Perez";
+            persona2.Edad = 28;
+            persona2.Estatura = 1.78;
+            persona2.Peso = 60.5;
+            persona2.Salario = 3000000;
+
+            persona2.infoPersona();
+
+            System.Console.WriteLine(persona1.indiceMasaC());
+            System.Console.WriteLine(persona2.indiceMasaC());
+            
+            persona1.alertaIMC(persona1.indiceMasaC());
+            persona2.alertaIMC(persona2.indiceMasaC());
+
+            Persona persona3 = new Persona("Daniel", "Benavides", 30, 1.70, 65, 0);
+            persona3.infoPersona();
+            persona3.alertaIMC(persona3.indiceMasaC());
+
+            
         }
     }
 }
